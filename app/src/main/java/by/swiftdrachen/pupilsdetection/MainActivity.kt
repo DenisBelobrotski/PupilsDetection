a54bdea012f6fb547f5d067c80dd1f230fcae987
DenisBelobrotski <belobrotskiy@gmail.com>
2021-03-17 21:58:39 +0300

    Obsolete code removed. Layouts fixed

--- app/src/main/java/by/swiftdrachen/pupilsdetection/MainActivity.kt
+++ app/src/main/java/by/swiftdrachen/pupilsdetection/MainActivity.kt
@@ -5,12 +5,10 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import by.swiftdrachen.pupilsdetection.obsolete.VideoDetectorActivity
 import by.swiftdrachen.pupilsdetection.tracking.cv_util.OpenCvUtils
 
 class MainActivity : AppCompatActivity() {
     private val imageDetectorButton by lazy { findViewById<Button>(R.id.image_detector_button) }
-    private val videoDetectorButton by lazy { findViewById<Button>(R.id.video_detector_button) }
     private val cameraDetectorButton by lazy { findViewById<Button>(R.id.camera_detector_button) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,11 +21,6 @@ class MainActivity : AppCompatActivity() {
         imageDetectorButton.setOnClickListener {
             val imageDetectorIntent = Intent(this, ImageDetectorActivity::class.java)
             startActivity(imageDetectorIntent)
-        }
-
-        videoDetectorButton.setOnClickListener {
-            val videoDetectorIntent = Intent(this, VideoDetectorActivity::class.java)
-            startActivity(videoDetectorIntent)
         }
 
         cameraDetectorButton.setOnClickListener {
